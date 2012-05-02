739dd174362334e6d5ede19d29dd65916d559a95
Simon Josi <simon.josi@atizo.com>
2012-05-02 13:43:17 +0200

    add class dependencies

--- manifests/git.pp
+++ manifests/git.pp
@@ -1,5 +1,6 @@
 class hudson::git {
   include git
+  Class['hudson::git'] <- Class['hudson']
   exec{'hudson_ssh_keypair':
     user => $hudson::user,
     command => "ssh-keygen -f ${hudson::home}/.ssh/id_rsa -t rsa -N ''",
