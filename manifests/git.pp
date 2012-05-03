a8484b61b21f1f2d2e1745825b2fc8f04a6aa84b
Simon Josi <simon.josi@atizo.com>
2012-05-03 18:03:38 +0200

    ensure .ssh directory for hudson user exists

--- manifests/git.pp
+++ manifests/git.pp
@@ -1,14 +1,19 @@
 class hudson::git {
   include git
   Class['hudson::git'] <- Class['hudson']
+  file{"${hudson::home}/.ssh":
+    ensure => directory,
+    owner => hudson, group => hudson, mode => 0700;
+  }
   exec{'hudson_ssh_keypair':
     user => $hudson::user,
     command => "ssh-keygen -f ${hudson::home}/.ssh/id_rsa -t rsa -N ''",
+    require => File["${hudson::home}/.ssh"],
     creates => "${hudson::home}/.ssh/id_rsa",
   }
   file{"${hudson::home}/.ssh/config":
     content => 'StrictHostKeyChecking no',
-    require => Exec['hudson_ssh_keypair'],
+    require => File["${hudson::home}/.ssh"],
     owner => hudson, group => hudson, mode => 0444;
   }
 }
